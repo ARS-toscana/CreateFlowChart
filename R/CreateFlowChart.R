49d13aab414465d9f300cc2056397eb90bd83c9c
GiorgioLimoncella <giorgio.limoncella@ars.toscana.it>
2021-05-25 17:47:05 +0200

    added variable figure

--- R/CreateFlowChart.R
+++ R/CreateFlowChart.R
@@ -11,7 +11,7 @@
 #' @param flowchartname: filename (possibly with path) of the output dataset containing the flowchart
 
 
-CreateFlowChart<-function(dataset,listcriteria,weight,strata,flowchartname, MultipleFlow=F) {
+CreateFlowChart<-function(dataset,listcriteria,weight,strata,flowchartname, figure=F, MultipleFlow=F) {
   if (!require("data.table")) install.packages("data.table")
   library(data.table)
   #create a list containing the names of the new columns
@@ -47,49 +47,48 @@ CreateFlowChart<-function(dataset,listcriteria,weight,strata,flowchartname, Mult
   #############################################################################
   ########################       Adding Figure       ##########################
   #############################################################################
-  if(!MultipleFlow){
-    x=rep("Flow", nrow(FLOW))
-    FlowChart<-data.frame(cbind(x, FLOW))
-  }else{
-    FlowChart<-data.frame(FLOW)
-    colnames(FlowChart)[1]<-"x"
-  }
-
-
-  n_criteria<-ncol(FlowChart)-2
-  nrow<-nrow(FlowChart)
-  criteria<-names(FlowChart)[2:(n_criteria+1)]
-
-  for(i in criteria){
-    for(j in seq(1, nrow)){
-      FlowChart[j, i]=FlowChart[j, i]-2
-    }
-  }
-
-  FlowChart$Included=-3
-  for(i in seq(1, nrow)){
-    row=FlowChart[i, ][2:(n_criteria+1)]
-    criteria_position<-(n_criteria+2) - length(row[row==-2])
-    if(length(row[row==-2]) == 0){
-      FlowChart[i, "Included" ]=FlowChart[i, "N"]
+  if(figure){
+    if(!MultipleFlow){
+      x=rep("Flow", nrow(FLOW))
+      FlowChart<-data.frame(cbind(x, FLOW))
     }else{
-      FlowChart[i, criteria_position ] = FlowChart[i, "N"]
+      FlowChart<-data.frame(FLOW)
+      colnames(FlowChart)[1]<-"x"
     }
+
+    n_criteria<-ncol(FlowChart)-2
+    nrow<-nrow(FlowChart)
+    criteria<-names(FlowChart)[2:(n_criteria+1)]
+
+    for(i in criteria){
+      for(j in seq(1, nrow)){
+        FlowChart[j, i]=FlowChart[j, i]-2
+      }
+    }
+
+    FlowChart$Included=-3
+    for(i in seq(1, nrow)){
+      row=FlowChart[i, ][2:(n_criteria+1)]
+      criteria_position<-(n_criteria+2) - length(row[row==-2])
+      if(length(row[row==-2]) == 0){
+        FlowChart[i, "Included" ]=FlowChart[i, "N"]
+      }else{
+        FlowChart[i, criteria_position ] = FlowChart[i, "N"]
+      }
+    }
+
+    FlowChart<-data.table(FlowChart)
+    FlowChart_reshaped<-melt(FlowChart, measure.vars = c(criteria, "Included") )
+    FlowChart_reshaped_plot<-FlowChart_reshaped[value>=0,][,-"value"]
+
+    ### Plot
+    flow_plot <-  ggplot(FlowChart_reshaped_plot, aes(x, N, fill=variable))+
+      geom_col(position="fill")+
+      scale_y_continuous(labels = scales::percent_format())
+
+    ### Saving the plot
+    ggsave(filename=paste0(dirfigure, flowchartname, "_figure.pdf"), plot=flow_plot)
   }
-
-
-  FlowChart<-data.table(FlowChart)
-  FlowChart_reshaped<-melt(FlowChart, measure.vars = c(criteria, "Included") )
-  FlowChart_reshaped_plot<-FlowChart_reshaped[value>=0,][,-"value"]
-
-
-  ### Plot
-
-  flow_plot <-  ggplot(FlowChart_reshaped_plot, aes(x, N, fill=variable))+
-    geom_col(position="fill")+
-    scale_y_continuous(labels = scales::percent_format())
-  ### Saving the plot
-  ggsave(filename=paste0(dirfigure, flowchartname, "_figure.pdf"), plot=flow_plot)
   #############################################################################
   ######################       End Adding Figure       ########################
   #############################################################################
